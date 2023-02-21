c475be6e4e5dc3d69bf9ed6af91d9d3e68812506
Jaysonzzr <jaysonzr2002@gmail.com>
2023-02-20 19:22:20 -0500

    Level1

---
+++ Guardian Breakout/Assets/Scripts/WallBreaker.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallBreaker : MonoBehaviour
+{
+    public float waitTime = 5;
+    private float originalWaitTime;
+    public float maxDistance = 5;
+    void Start()
+    {
+        originalWaitTime = waitTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(Input.GetMouseButton(0)){
+            waitTime -= Time.deltaTime;
+        }
+        if(Input.GetMouseButtonUp(0)){
+            waitTime = originalWaitTime;
+        }
+        if(waitTime <= 0){
+            checkForWallBreak();
+            waitTime = originalWaitTime;
+        }
+        
+        
+        
+    }
+
+    void checkForWallBreak(){
+        if(Physics.Raycast(transform.position,transform.forward, out RaycastHit hit, maxDistance)){
+            if(hit.collider.CompareTag("Breakable")){
+                Destroy(hit.transform.gameObject);
+            }
+        }
+    }
+}
