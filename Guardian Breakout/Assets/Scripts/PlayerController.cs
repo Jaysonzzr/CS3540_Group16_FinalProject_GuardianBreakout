ea8aede7d7ea6985738ab67ff03d2882989f9828
Jaysonzzr <jaysonzr2002@gmail.com>
2023-02-20 23:33:44 -0500

    Level1

--- Guardian Breakout/Assets/Scripts/PlayerController.cs
+++ Guardian Breakout/Assets/Scripts/PlayerController.cs
@@ -31,24 +31,12 @@ public class PlayerController : MonoBehaviour
         if(controller.isGrounded)
         {
             moveDirection = input;
-
-            // we can jump
-            if (Input.GetButton("Jump"))
-            {
-                moveDirection.y = Mathf.Sqrt(2 * jumpHeight * gravity);
-            }
-            else
-            {
-                moveDirection.y = 0.0f;
-            }
         }
         else
         {
-            // we are midair
             input.y = moveDirection.y;
             moveDirection = Vector3.Lerp(moveDirection, input, airControl * Time.deltaTime);
         }
-
         moveDirection.y -= gravity * Time.deltaTime;
 
         controller.Move(input * Time.deltaTime);
