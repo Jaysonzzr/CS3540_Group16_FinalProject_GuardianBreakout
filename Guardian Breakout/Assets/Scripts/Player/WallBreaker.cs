9ddfeeb1a3fec5eb5aadaeccdee2740d1ab22be3
Jaysonzzr <jaysonzr2002@gmail.com>
2023-04-02 14:42:50 -0400

    Wall Breaker Bug Fixed && Level Design

--- Guardian Breakout/Assets/Scripts/Player/WallBreaker.cs
+++ Guardian Breakout/Assets/Scripts/Player/WallBreaker.cs
@@ -13,41 +13,64 @@ public class WallBreaker : MonoBehaviour
     float currentProcess;
 
     public InventoryManager inventoryManager;
+    
+    Transform pickaxe;
+    Animator pickaxeAnim;
+    Quaternion pickaxeRot;
+
+    // Add a variable to store the last breakable object
+    private GameObject lastBreakableObject;
 
     void Start()
     {
         originalWaitTime = waitTime;
         inventoryManager = GameObject.Find("LevelManager").GetComponent<InventoryManager>();
+
+        pickaxe = Camera.main.transform.Find("Pickaxe").transform;
+        pickaxeAnim = pickaxe.GetComponent<Animator>();
+        pickaxeRot = pickaxe.localRotation;
     }
 
     // Update is called once per frame
     void Update()
     {
         processBar.value = currentProcess;
-        if(Physics.Raycast(transform.position,transform.forward, out RaycastHit hit, maxDistance))
+        if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, maxDistance))
         {
             GameObject hitObject = hit.transform.gameObject;
             if (hitObject.CompareTag("Breakable"))
             {
                 if (inventoryManager.holdStuff && inventoryManager.holding.name == "Pickaxe")
                 {
-                    if(Input.GetMouseButton(0))
+                    // If the breakable object changed, reset the process
+                    if (lastBreakableObject != null && lastBreakableObject != hitObject)
+                    {
+                        waitTime = originalWaitTime;
+                        currentProcess = 0.0f;
+                        lastBreakableObject.transform.GetComponent<Outline>().enabled = false;
+                    }
+
+                    if (Input.GetMouseButton(0))
                     {
                         waitTime -= Time.deltaTime;
                         hitObject.GetComponent<Outline>().enabled = true;
                         currentProcess = (1 - waitTime / originalWaitTime) * 100;
                         processBar.gameObject.SetActive(true);
+
+                        pickaxeAnim.SetInteger("is_attacking", 1);
                     }
-                    
-                    if(Input.GetMouseButtonUp(0))
+                    else
                     {
                         waitTime = originalWaitTime;
                         currentProcess = 0.0f;
                         hitObject.GetComponent<Outline>().enabled = false;
                         processBar.gameObject.SetActive(false);
+
+                        pickaxe.localRotation = Quaternion.Lerp(pickaxe.localRotation, pickaxeRot, Time.deltaTime * 5);
+                        pickaxeAnim.SetInteger("is_attacking", 0);
                     }
-                    
-                    if(waitTime <= 0)
+
+                    if (waitTime <= 0)
                     {
                         //Destroy(hit.transform.gameObject);
                         hit.transform.GetComponent<MeshRenderer>().enabled = false;
@@ -56,7 +79,24 @@ public class WallBreaker : MonoBehaviour
                         processBar.gameObject.SetActive(false);
                         waitTime = originalWaitTime;
                     }
+
+                    // Update the last breakable object
+                    lastBreakableObject = hitObject;
                 }
+            }
+            else
+            {
+                if (lastBreakableObject != null)
+                {
+                    lastBreakableObject.transform.GetComponent<Outline>().enabled = false;
+                }
+
+                waitTime = originalWaitTime;
+                currentProcess = 0.0f;
+                processBar.gameObject.SetActive(false);
+
+                pickaxe.localRotation = Quaternion.Lerp(pickaxe.localRotation, pickaxeRot, Time.deltaTime * 5);
+                pickaxeAnim.SetInteger("is_attacking", 0);
             }
         }
         else
